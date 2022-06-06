c7fc119c2fda1a1fbf8a5bac4ed4a1435aaaf58f
Chris Williams <cwi@juxt.pro>
2022-06-06 09:44:24 +0100

    Fix issue importing from ics files with hyphenated tags

--- src/juxt/time/adapters/ics_adapter.clj
+++ src/juxt/time/adapters/ics_adapter.clj
@@ -1,8 +1,13 @@
+;; Copyright © 2022, JUXT LTD.
+
 (ns juxt.time.adapters.ics-adapter
   (:require [tick.core :as t]
+            [tick.alpha.interval :as t.i]
             [tick.alpha.ical :as ical]
             [clojure.java.io :as io]))
 
+
+(defmethod ical/coerce-to-value "DURATION" [_ value] (. java.time.Duration parse value))
 
 (defn read-data
   [file-path options]
@@ -18,4 +23,10 @@
 
 (read-data "/var/tmp/england-and-wales.ics" {})
 (read-data "/var/tmp/germany.ics" {})
+(t.i/interval "PT48H")
+(t/new-duration "PT48H")
+
+(. java.time.Duration parse "PT48H")
+(t/duration "PT48H")
+
   )
